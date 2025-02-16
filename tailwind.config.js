197e4c69aa8fc223c60a65054f3be8cc84719c98
Srinila-04 <srinila042005@gmail.com>
2025-02-16 22:08:28 +0530

    update project

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: ["./src/**/*.html", "./src/**/*.js"], 
+  darkMode: false, 
+  theme: { extend: {} },
+  variants: { extend: {} },
+  plugins: [],
+};
+
+
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        merriweather: ["Merriweather", "serif"],
+      },
+    },
+  },
+  plugins: [],
+};
